f1f1d495b0eacb061397966bbe9f61f4fcd065a2
andela-jkanyua <jimnah.magira@andela.com>
2018-04-05 13:06:45 +0300

    Feature/setup login component (#8)
    * setup login component, redux
    - setup redux store
    - setup action, action creators, reducers for redux store
    - add util and config files
    - added packages redux, react-redux, superagent
    
    * add allow cors
    
    * remove console.log

---
+++ client/src/Auth/AuthRedux.js
@@ -0,0 +1,89 @@
+import request from 'superagent';
+import { config } from '../config'
+import * as localStorage from '../utils'
+/*
+ * ----------------- Action Types -------------------
+ */
+
+export const types = {
+  LOGIN_REQUEST: 'LOGIN_REQUEST',
+  LOGIN_SUCCESS: 'LOGIN_SUCCESS',
+  LOGIN_FAILURE: 'LOGIN_FAILURE'
+}
+
+/*
+ * ----------------- Action Creators -------------------
+ */
+function loginRequest(requestedAt) {
+  return {
+    type: types.LOGIN_REQUEST,
+    requestedAt
+  }
+}
+
+function loginSuccess(token){
+  return {
+    type: types.LOGIN_SUCCESS,
+    token,
+    isLoggedIn: true
+  }
+}
+
+function loginFailure(err){
+  return {
+    type: types.LOGIN_FAILURE,
+    error: err.message,
+    isLoggedIn: false
+  }
+}
+
+
+export function login(creds) {
+  return (dispatch) => {
+    const requestedAt = Date.now()
+    dispatch(loginRequest(requestedAt));
+    return (
+      request
+        .post(`${config.base_url}user/login`)
+        .send(creds)
+        .then((response) => {
+          localStorage.setToken(response.body.token);
+          dispatch(loginSuccess(response.body));
+        }).catch((err) => {
+          dispatch(loginFailure(err));
+        })
+    );
+  };
+}
+
+/*
+ * ----------------- Reducers -------------------
+ */
+
+ const initialState = {
+   isLoggedIn: false,
+   requestedAt: null,
+   error: null
+ }
+
+ export default function authReducer(state=initialState, action) {
+   switch(action.type) {
+     case types.LOGIN_REQUEST:
+     return Object.assign({}, state, {
+       requestedAt: action.requestedAt
+      })
+      case types.LOGIN_SUCCESS:
+      return Object.assign({}, state, {
+        token: action.token,
+        isLoggedIn: action.isLoggedIn,
+        error: null
+      })
+      case types.LOGIN_FAILURE:
+      return Object.assign({}, state, {
+        error: action.error,
+        isLoggedIn: action.isLoggedIn,
+      })
+      default:
+      return state
+   }
+ }
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Provider } from 'react-redux'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import store from './store'
+
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 const Wrapper = () =>
+<Provider store={store}>
   <MuiThemeProvider>
     <App />
   </MuiThemeProvider>
+</Provider>
 
 ReactDOM.render(<Wrapper />, document.getElementById('root'));
 registerServiceWorker();
