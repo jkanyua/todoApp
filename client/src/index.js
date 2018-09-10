0d5f9e7e4ee9415464e3c7d60411f3bcca6b2cf3
Jimnah Magira <jimnah.magira@andela.com>
2018-09-11 01:00:06 +0300

    Upgrade packages/remove tap-event-plugin (#14)

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux'
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -10,7 +9,6 @@ import store from './store'
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
 
 const Wrapper = () =>
 <Provider store={store}>
